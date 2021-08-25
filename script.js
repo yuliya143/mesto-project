2f97adc216feb385f0ac940b4b5bdf025d5da556
Julia Zabolotnaya <yuv.zabolotnaya@yandex.by>
2021-08-25 09:35:22 +0300

    Add cards and likes logic

--- script.js
+++ script.js
@@ -33,3 +33,67 @@ function submitForm(evt) {
 
   closePopup();
 }
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
+
+function createCard(place) {
+  const str = `<li class="place">
+  <img
+    src="${place.link}"
+    alt="${place.name}"
+    class="place__image"
+  />
+  <div class="place__content">
+    <h2 class="place__title">${place.name}</h2>
+    <button type="button" class="place__like-button button">
+      <svg class="place__icon" fill="currentColor">
+        <use xlink:href="#heart" />
+      </svg>
+    </button>
+  </div>
+</li>`;
+  return str;
+}
+
+let places = '';
+for (let i = 0; i < initialCards.length; i++) {
+  places += createCard(initialCards[i]);
+}
+console.log(places);
+
+const list = document.querySelector('.gallery__list');
+list.innerHTML = places;
+
+const likeButton = document.querySelectorAll('.place__like-button');
+
+for (let i = 0; i < likeButton.length; i++) {
+  likeButton[i].addEventListener('click', (evt) => {
+    console.log(evt);
+    evt.currentTarget.firstElementChild.classList.toggle('place__icon_active');
+  });
+}
